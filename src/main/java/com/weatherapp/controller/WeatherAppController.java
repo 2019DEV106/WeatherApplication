787640f7c1ce1e31352ec8aeb3e2aad80d804205
“Krishna” <iamkrishv@gmail.com>
2019-02-14 15:41:53 +0100

    Added negative and positive test cases in the controller test class along with its corresponding controller class

--- src/main/java/com/weatherapp/controller/WeatherAppController.java
+++ src/main/java/com/weatherapp/controller/WeatherAppController.java
@@ -1,30 +1,37 @@
 package com.weatherapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.weatherapp.customexception.WeatherException;
+import com.weatherapp.service.WeatherAppServiceImpl;
+
 
 @Controller
 public class WeatherAppController {
-	 
-	
-	@GetMapping(value="/weatherReport")
-	public String weatherReport()  {
+
+	@Autowired
+	public WeatherAppServiceImpl weatherAppServiceImpl;
+
+	@GetMapping(value = "/weatherReport")
+	public String weatherReport() {
 
 		return "weatherInfo";
-	}   
-	
-	
-	@PostMapping(value="/weatherReport")
-	public String prepareWeatherReport(ModelMap model,@RequestParam String latitude , @RequestParam String longitude){
-		  
-		return "weatherDetails";
 	}
 
- 
-	 
-	 
+	@PostMapping(value = "/weatherReport")
+	public String prepareWeatherReport(ModelMap model, @RequestParam String latitude, @RequestParam String longitude) {
+		String latitudeAndlongitude = "lat=" + latitude + "&lon=" + longitude;
+		try {
+			model.put("weatherData", weatherAppServiceImpl.fetchweatherInfo(latitudeAndlongitude));
+			return "weatherDetails";
+		} catch (WeatherException e) {
+			return "errorPage";
+		}
+	}
+
 }
--- src/test/java/com/weatherapp/controller/WeatherAppControllerTest.java
+++ src/test/java/com/weatherapp/controller/WeatherAppControllerTest.java
@@ -1,6 +1,12 @@
 package com.weatherapp.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -9,17 +15,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.ModelMap;
+
+import com.weatherapp.customexception.WeatherException;
+import com.weatherapp.dto.WeatherData;
+import com.weatherapp.service.WeatherAppServiceImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(WeatherAppController.class)
 public class WeatherAppControllerTest {
 
+	@MockBean
+	public WeatherAppServiceImpl weatherAppServiceImpl;
+
+	@Mock
+	public WeatherAppServiceImpl weatherAppServiceImplMock;
+	
 	 
 	@InjectMocks
 	private WeatherAppController weatherAppController;
@@ -78,4 +97,38 @@ public class WeatherAppControllerTest {
 				.andExpect(status().isOk());
 	}
 	
+	@Test
+	public void testWeatherReportWeatherException() throws Exception{
+		when(weatherAppServiceImplMock.fetchweatherInfo(anyString())).thenThrow(new WeatherException("exception"));
+		ModelMap modelMap = new ModelMap();
+		String result = weatherAppController.prepareWeatherReport(modelMap, "latitude", "12350.8503");
+		assertEquals("errorPage", result);
+		assertNull(modelMap.get("weatherData"));
+		verify(weatherAppServiceImplMock, times(1)).fetchweatherInfo(anyString());
+	}
+	
+	@Test
+	public void testWeatherReportReturnNullResponse() throws Exception{
+		when(weatherAppServiceImplMock.fetchweatherInfo("lat=50.8503&lon=4.3517")).thenReturn(null);
+		ModelMap modelMap = new ModelMap();
+		String result = weatherAppController.prepareWeatherReport(modelMap, "latitude", "12350.8503");
+		assertEquals("weatherDetails", result);
+		assertNull(modelMap.get("weatherData"));
+		verify(weatherAppServiceImplMock, times(1)).fetchweatherInfo(anyString());
+	}
+	
+	@Test
+	public void testWeatherReportReturnSuccessResponse() throws Exception{
+		WeatherData weatherData = new WeatherData();
+		weatherData.setName("Brussels");
+		when(weatherAppServiceImplMock.fetchweatherInfo(anyString())).thenReturn(weatherData);
+
+		ModelMap modelMap = new ModelMap();
+		String result = weatherAppController.prepareWeatherReport(modelMap, "latitude", "12350.8503");
+		assertEquals("weatherDetails", result);
+		assertNotNull(modelMap.get("weatherData"));
+		assertEquals("Brussels", weatherData.getName());
+		verify(weatherAppServiceImplMock, times(1)).fetchweatherInfo(anyString());
+	}
+
 }

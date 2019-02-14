177a9a65d8d020c10a6a955107d5c46c473c6806
“Krishna” <iamkrishv@gmail.com>
2019-02-14 11:58:45 +0100

    initial project setup with spring boot application

---
+++ src/test/java/com/weatherapp/SpringBootAppTest.java
@@ -0,0 +1,15 @@
+package com.weatherapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.weatherapp.SpringBootApp;
+@RunWith(SpringRunner.class)
+public class SpringBootAppTest {
+
+	@Test
+	public void testMain() {
+		SpringBootApp.main(new String[] {});
+	}
+}

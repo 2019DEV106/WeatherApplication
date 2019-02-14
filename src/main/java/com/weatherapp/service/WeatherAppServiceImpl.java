cd8b6a338d796a24de88b6fc1c32608690de217b
“Krishna” <iamkrishv@gmail.com>
2019-02-14 17:16:02 +0100

    Added negative testcases in the serviceImplementaionTest along with the service implementation classes

--- src/main/java/com/weatherapp/service/WeatherAppServiceImpl.java
+++ src/main/java/com/weatherapp/service/WeatherAppServiceImpl.java
@@ -1,19 +1,42 @@
 package com.weatherapp.service;
 
-import org.springframework.stereotype.Service;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.weatherapp.constants.WeatherAppConstants;
 import com.weatherapp.customexception.WeatherException;
 import com.weatherapp.dto.WeatherData;
 
 @Service
 public class WeatherAppServiceImpl implements WeatherAppService {
 
-	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private ObjectMapper mapper;
 
 	@Override
-	public WeatherData fetchweatherInfo(String latitudeAndlongitude) throws WeatherException { 
-		
-		return null;
+	public WeatherData fetchweatherInfo(String latitudeAndlongitude) throws WeatherException {
+
+		String uri = WeatherAppConstants.WHEATHER_API_URI + latitudeAndlongitude;
+		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+		try {
+			return mapper.readValue(response.getBody(), WeatherData.class);
+		} catch (JsonMappingException e) {
+			throw new WeatherException("Exception during Json Mapping ", e);
+		} catch (JsonParseException e) {
+			throw new WeatherException("Exception during Json Parsing ", e);
+		} catch (IOException e) {
+			throw new WeatherException("IOException during API call ", e);
+		}
 	}
 
 }
--- src/test/java/com/weatherapp/service/WeatherAppServiceImplTest.java
+++ src/test/java/com/weatherapp/service/WeatherAppServiceImplTest.java
@@ -1,16 +1,28 @@
 package com.weatherapp.service;
 
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weatherapp.constants.WeatherAppConstants;
+import com.weatherapp.customexception.WeatherException;
 import com.weatherapp.dto.WeatherData;
 
 public class WeatherAppServiceImplTest {
@@ -18,6 +30,9 @@ public class WeatherAppServiceImplTest {
 	@Mock
 	private RestTemplate restTemplate;
 
+	@Mock
+	private ObjectMapper objectMapper;
+	
 	@InjectMocks
 	private WeatherAppServiceImpl weatherAppService;
 	
@@ -26,7 +41,7 @@ public class WeatherAppServiceImplTest {
 		MockitoAnnotations.initMocks(this);
 	}
 	
-	@Test
+	@Test(expected=NullPointerException.class)
 	public void testfetchweatherInfoReturnNullResponse() throws Exception {
 		when(restTemplate.getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class))
 		.thenReturn(null);
@@ -34,4 +49,52 @@ public class WeatherAppServiceImplTest {
 		assertNull(weatherData);
 	}
 	 
+	@Test(expected=WeatherException.class)
+	public void testJsonMappingException() throws Exception {
+		ResponseEntity<String> response = new ResponseEntity<String>("not a json", HttpStatus.OK);
+		when(restTemplate.getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class))
+				.thenReturn(response);
+		when(objectMapper.readValue("not a json", WeatherData.class)).thenThrow((JsonMappingException.class));
+		weatherAppService.fetchweatherInfo("lat=50.8503&lon=4.3517");
+		verify(restTemplate, times(1)).getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class);
+		verify(objectMapper, times(1)).readValue("not a json", WeatherData.class);
+
+	}
+	
+	@Test(expected=WeatherException.class)
+	public void testJsonParsingException() throws Exception {
+		ResponseEntity<String> response = new ResponseEntity<String>("not a json", HttpStatus.OK);
+		when(restTemplate.getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class))
+				.thenReturn(response);
+		when(objectMapper.readValue("not a json", WeatherData.class)).thenThrow((JsonParseException.class));
+		weatherAppService.fetchweatherInfo("lat=50.8503&lon=4.3517");
+		verify(restTemplate, times(1)).getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class);
+		verify(objectMapper, times(1)).readValue("not a json", WeatherData.class);
+
+	}
+	
+	@Test(expected=WeatherException.class)
+	public void testIOExceptionduringJsonParsing() throws Exception {
+		ResponseEntity<String> response = new ResponseEntity<String>("not a json", HttpStatus.OK);
+		when(restTemplate.getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class))
+				.thenReturn(response);
+		when(objectMapper.readValue("not a json", WeatherData.class)).thenThrow((IOException.class));
+		weatherAppService.fetchweatherInfo("lat=50.8503&lon=4.3517");
+		verify(restTemplate, times(1)).getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class);
+		verify(objectMapper, times(1)).readValue("not a json", WeatherData.class);
+	}
+
+	@Test(expected= RestClientException.class)
+	public void testRestClientExceptionDuringAPICall() throws Exception {
+		when(restTemplate.getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class))
+				.thenThrow(RestClientException.class);
+
+		weatherAppService.fetchweatherInfo("lat=50.8503&lon=4.3517");
+		verify(restTemplate, times(1)).getForEntity(WeatherAppConstants.WHEATHER_API_URI+"lat=50.8503&lon=4.3517", String.class);
+		verifyZeroInteractions(objectMapper);
+
+	}
+	
+
+
 }
